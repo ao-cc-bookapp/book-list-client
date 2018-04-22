c04e34896a0bef6e1487c24744b628da6d6ee1dd
Christopher Chew <fishkepr@comcast.net>
2018-04-21 23:25:32 -0700

    Create New Book Working

--- scripts/views/create-book-view.js
+++ scripts/views/create-book-view.js
@@ -2,66 +2,6 @@
 
 var app = app || {};
 
-// Displays a single Book
-(function(module) {
-    const singleBookView = {};
-    console.log('Entering singleBookView');
-    
-  
-    singleBookView.initSinglePage = function() {
-        console.log('Entering singleBookView.initSinglePage');
-      $('.container').hide();
-      $('#detail-view').show();
-      module.Book.all.map(book => $('#book-list').append(book.toHtml()));
-    };
-  
-    module.singleBookView = singleBookView;
-  })(app);
-  
-//   $(function() {
-//     console.log('Calling fetchOne from single-book-view.js');
-//     app.Book.fetchOne(app.singleBookView.initIndexPage);
-//   }); 
-
-// Displays all Books
-// (function(module) {
-//   const bookView = {};
-
-//   bookView.initIndexPage = function() { 
-//     console.log('running  initIndexPage THIRD');
-//     $('.container').hide();
-//     $('#book-view').show();
-//     module.Book.all.map(book => $('#book-list').append(book.toHtml()));
-//   };
-
-//   // app.Book.fetchAll(app.bookView.initIndexPage);
-
-//   console.log('instantiating bookView FIRST');
-
-
-//   console.log('Setting up the listener');
-//   $('#book-view ul').off()
-  
-//   $('#book-view ul').on('click', '.book-container', (event) => {
-//     console.log('Click detected');
-//     const id = $(event.target).parent().data('book_id');
-//     // const id = '1';
-//     page('/books/' + id)
-//   })
-
-//   module.bookView = bookView;
-
-// })(app);
-
-
-
-// $(function() {
-//   console.log('Calling fetchAll from book-view.js FOURTH');
-//   app.Book.fetchAll(app.bookView.initIndexPage);
-// });
-
-// console.log('This is outside the function in question! SECOND');
-
 // Sends the create form
 (function(module) {
   const createBookView = {};
@@ -72,12 +12,16 @@ var app = app || {};
     $('#form-view').show();
     $('#create-book').off().on('submit', 'form', (event) => {
         event.preventDefault();
-        const title = $('#title').val();
-        const author = $('#author').val();
-        const isbn = $('#isbn').val();
-        const image_url = $('#image-url').val();
-        const description = $('#book-description').val();
-        app.Thing.create({ name }).then(page('/'));
+        console.log('SUBMIT PRESSED');
+        let newBook = {
+            title:$('#title').val(),
+            author:$('#author').val(),
+            isbn:$('#isbn').val(),
+            image_url:$('#image-url').val(),
+            description:$('#book-description').val()
+        }
+        console.log('newBook: ',newBook);
+        app.Book.create(newBook).then(page('/'));
         $('#create-form-name').val('');
 })
     // $('#single-book').empty();
